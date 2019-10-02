pulllock/spring-boot

commit e41c5a43273cab9dc2ce0c1c1e86ca7c633634c8

Require annotation signal for constructor binding
Update `@ConfigurationProperties` constructor binding support to only
apply when a `@ConstructorBinding` annotation is present on either the
type or the specific constructor to use.

Prior to this commit we didn't have a good way to tell when constructor
binding should be used vs regular autowiring.

For convenience, an `@ImmutableConfigurationProperties` meta-annotation
has also been added which is composed of `@ConfigurationProperties` and
`@ConstructorBinding`.

Closes gh-18469

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/ValueObjectBinder.java
@@ -22,6 +22,7 @@ import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 import kotlin.reflect.KFunction;
 import kotlin.reflect.KParameter;
@@ -106,9 +107,9 @@ class ValueObjectBinder implements DataObjectBinder {
 				return null;
 			}
 			if (KotlinDetector.isKotlinType(type)) {
-				return KotlinValueObject.get(type);
+				return KotlinValueObject.get(type, bindable.getConstructorFilter());
 			}
-			return DefaultValueObject.get(type);
+			return DefaultValueObject.get(type, bindable.getConstructorFilter());
 		}
 
 	}
@@ -142,9 +143,10 @@ class ValueObjectBinder implements DataObjectBinder {
 			return this.constructorParameters;
 		}
 
-		static <T> ValueObject<T> get(Class<T> type) {
+		static <T> ValueObject<T> get(Class<T> type, Predicate<Constructor<?>> constructorFilter) {
 			Constructor<T> primaryConstructor = BeanUtils.findPrimaryConstructor(type);
-			if (primaryConstructor == null || primaryConstructor.getParameterCount() == 0) {
+			if (primaryConstructor == null || primaryConstructor.getParameterCount() == 0
+					|| !constructorFilter.test(primaryConstructor)) {
 				return null;
 			}
 			KFunction<T> kotlinConstructor = ReflectJvmMapping.getKotlinFunction(primaryConstructor);
@@ -191,18 +193,23 @@ class ValueObjectBinder implements DataObjectBinder {
 		}
 
 		@SuppressWarnings("unchecked")
-		static <T> ValueObject<T> get(Class<T> type) {
+		static <T> ValueObject<T> get(Class<T> type, Predicate<Constructor<?>> constructorFilter) {
 			Constructor<?> constructor = null;
 			for (Constructor<?> candidate : type.getDeclaredConstructors()) {
-				int modifiers = candidate.getModifiers();
-				if (!Modifier.isPrivate(modifiers) && !Modifier.isProtected(modifiers)) {
+				if (isCandidateConstructor(candidate, constructorFilter)) {
 					if (constructor != null) {
 						return null;
 					}
 					constructor = candidate;
 				}
 			}
-			return get((Constructor<T>) constructor);
+			return (constructor != null) ? new DefaultValueObject<>((Constructor<T>) constructor) : null;
+		}
+
+		private static boolean isCandidateConstructor(Constructor<?> candidate, Predicate<Constructor<?>> filter) {
+			int modifiers = candidate.getModifiers();
+			return !Modifier.isPrivate(modifiers) && !Modifier.isProtected(modifiers)
+					&& candidate.getParameterCount() > 0 && filter.test(candidate);
 		}
 
 		static <T> DefaultValueObject<T> get(Constructor<T> constructor) {

spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/ConfigurationPropertiesTests.java
@@ -776,7 +776,6 @@ class ConfigurationPropertiesTests {
 	}
 
 	@Test
-	@SuppressWarnings("deprecation")
 	void loadWhenBindingOnBeanWithoutBeanDefinitionShouldBind() {
 		load(BasicConfiguration.class, "name=test");
 		BasicProperties bean = this.context.getBean(BasicProperties.class);
@@ -785,6 +784,19 @@ class ConfigurationPropertiesTests {
 		this.context.getBean(ConfigurationPropertiesBindingPostProcessor.class).postProcessBeforeInitialization(bean,
 				"does-not-exist");
 		assertThat(bean.name).isEqualTo("test");
+	}
+
+	@Test
+	void loadWhenBindingToNestedConstructorPropertiesShouldBind() {
+		MutablePropertySources sources = this.context.getEnvironment().getPropertySources();
+		Map<String, Object> source = new HashMap<>();
+		source.put("test.name", "spring");
+		source.put("test.nested.age", "5");
+		sources.addLast(new MapPropertySource("test", source));
+		load(NestedConstructorPropertiesConfiguration.class);
+		NestedConstructorProperties bean = this.context.getBean(NestedConstructorProperties.class);
+		assertThat(bean.getName()).isEqualTo("spring");
+		assertThat(bean.getNested().getAge()).isEqualTo(5);
 	}
 
 	private AnnotationConfigApplicationContext load(Class<?> configuration, String... inlinedProperties) {
@@ -1744,7 +1756,7 @@ class ConfigurationPropertiesTests {
 
 	}
 
-	@ConfigurationProperties(prefix = "test")
+	@ImmutableConfigurationProperties(prefix = "test")
 	static class OtherInjectedProperties {
 
 		final DataSizeProperties dataSizeProperties;
@@ -1761,7 +1773,7 @@ class ConfigurationPropertiesTests {
 
 	}
 
-	@ConfigurationProperties(prefix = "test")
+	@ImmutableConfigurationProperties(prefix = "test")
 	@Validated
 	static class ConstructorParameterProperties {
 
@@ -1785,7 +1797,7 @@ class ConfigurationPropertiesTests {
 
 	}
 
-	@ConfigurationProperties(prefix = "test")
+	@ImmutableConfigurationProperties(prefix = "test")
 	@Validated
 	static class ConstructorParameterValidatedProperties {
 
@@ -1894,6 +1906,49 @@ class ConfigurationPropertiesTests {
 
 		void setB(int b) {
 			this.b = b;
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@EnableConfigurationProperties(NestedConstructorProperties.class)
+	static class NestedConstructorPropertiesConfiguration {
+
+	}
+
+	@ImmutableConfigurationProperties("test")
+	static class NestedConstructorProperties {
+
+		private final String name;
+
+		private final Nested nested;
+
+		NestedConstructorProperties(String name, Nested nested) {
+			this.name = name;
+			this.nested = nested;
+		}
+
+		String getName() {
+			return this.name;
+		}
+
+		Nested getNested() {
+			return this.nested;
+		}
+
+		static class Nested {
+
+			private final int age;
+
+			@ConstructorBinding
+			Nested(int age) {
+				this.age = age;
+			}
+
+			int getAge() {
+				return this.age;
+			}
+
 		}
 
 	}

spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/web/client/ExampleProperties.java
@@ -16,16 +16,16 @@
 
 package org.springframework.boot.test.autoconfigure.web.client;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ImmutableConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 /**
- * Example {@link ConfigurationProperties} used to test the use of configuration
- * properties scan with sliced test.
+ * Example {@link ImmutableConfigurationProperties @ImmutableConfigurationProperties} used
+ * to test the use of configuration properties scan with sliced test.
  *
  * @author Stephane Nicoll
  */
-@ConfigurationProperties("example")
+@ImmutableConfigurationProperties("example")
 public class ExampleProperties {
 
 	private final String name;

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/ConfigurationPropertiesBean.java
@@ -17,12 +17,14 @@
 package org.springframework.boot.context.properties;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
@@ -32,8 +34,11 @@ import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.KotlinDetector;
 import org.springframework.core.ResolvableType;
-import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.annotation.MergedAnnotation;
+import org.springframework.core.annotation.MergedAnnotations;
+import org.springframework.core.annotation.MergedAnnotations.SearchStrategy;
 import org.springframework.util.Assert;
 import org.springframework.validation.annotation.Validated;
 
@@ -56,12 +61,15 @@ public final class ConfigurationPropertiesBean {
 
 	private final Bindable<?> bindTarget;
 
+	private final BindMethod bindMethod;
+
 	private ConfigurationPropertiesBean(String name, Object instance, ConfigurationProperties annotation,
-			Bindable<?> bindTarget) {
+			Bindable<?> bindTarget, BindMethod bindMethod) {
 		this.name = name;
 		this.instance = instance;
 		this.annotation = annotation;
 		this.bindTarget = bindTarget;
+		this.bindMethod = bindMethod;
 	}
 
 	/**
@@ -78,6 +86,22 @@ public final class ConfigurationPropertiesBean {
 	 */
 	public Object getInstance() {
 		return this.instance;
+	}
+
+	/**
+	 * Return the bean type.
+	 * @return the bean type
+	 */
+	Class<?> getType() {
+		return this.bindTarget.getType().resolve();
+	}
+
+	/**
+	 * Return the property binding method that was used for the bean.
+	 * @return the bind type
+	 */
+	public BindMethod getBindMethod() {
+		return this.bindMethod;
 	}
 
 	/**
@@ -152,18 +176,7 @@ public final class ConfigurationPropertiesBean {
 	 */
 	public static ConfigurationPropertiesBean get(ApplicationContext applicationContext, Object bean, String beanName) {
 		Method factoryMethod = findFactoryMethod(applicationContext, beanName);
-		ConfigurationProperties annotation = getAnnotation(applicationContext, bean, beanName, factoryMethod,
-				ConfigurationProperties.class);
-		if (annotation == null) {
-			return null;
-		}
-		ResolvableType type = (factoryMethod != null) ? ResolvableType.forMethodReturnType(factoryMethod)
-				: ResolvableType.forClass(bean.getClass());
-		Validated validated = getAnnotation(applicationContext, bean, beanName, factoryMethod, Validated.class);
-		Annotation[] annotations = (validated != null) ? new Annotation[] { annotation, validated }
-				: new Annotation[] { annotation };
-		Bindable<?> bindTarget = Bindable.of(type).withAnnotations(annotations).withExistingValue(bean);
-		return new ConfigurationPropertiesBean(beanName, bean, annotation, bindTarget);
+		return create(beanName, bean, bean.getClass(), factoryMethod);
 	}
 
 	private static Method findFactoryMethod(ApplicationContext applicationContext, String beanName) {
@@ -184,22 +197,105 @@ public final class ConfigurationPropertiesBean {
 		return null;
 	}
 
-	private static <A extends Annotation> A getAnnotation(ApplicationContext applicationContext, Object bean,
-			String beanName, Method factoryMethod, Class<A> annotationType) {
-		if (factoryMethod != null) {
-			A annotation = AnnotationUtils.findAnnotation(factoryMethod, annotationType);
-			if (annotation != null) {
-				return annotation;
+	static ConfigurationPropertiesBean forValueObject(Class<?> beanClass, String beanName) {
+		ConfigurationPropertiesBean propertiesBean = create(beanName, null, beanClass, null);
+		Assert.state(propertiesBean != null && propertiesBean.getBindMethod() == BindMethod.VALUE_OBJECT,
+				"Bean '" + beanName + "' is not a @ConfigurationProperties value object");
+		return propertiesBean;
+	}
+
+	private static ConfigurationPropertiesBean create(String name, Object instance, Class<?> type, Method factory) {
+		ConfigurationProperties annotation = findAnnotation(instance, type, factory, ConfigurationProperties.class);
+		if (annotation == null) {
+			return null;
+		}
+		Validated validated = findAnnotation(instance, type, factory, Validated.class);
+		Annotation[] annotations = (validated != null) ? new Annotation[] { annotation, validated }
+				: new Annotation[] { annotation };
+		Constructor<?> bindConstructor = BindMethod.findBindConstructor(type);
+		BindMethod bindMethod = (bindConstructor != null) ? BindMethod.VALUE_OBJECT : BindMethod.forClass(type);
+		ResolvableType bindType = (factory != null) ? ResolvableType.forMethodReturnType(factory)
+				: ResolvableType.forClass(type);
+		Bindable<Object> bindTarget = Bindable.of(bindType).withAnnotations(annotations)
+				.withConstructorFilter(ConfigurationPropertiesBean::isBindableConstructor);
+		if (instance != null) {
+			bindTarget = bindTarget.withExistingValue(instance);
+		}
+		return new ConfigurationPropertiesBean(name, instance, annotation, bindTarget, bindMethod);
+	}
+
+	private static <A extends Annotation> A findAnnotation(Object instance, Class<?> type, Method factory,
+			Class<A> annotationType) {
+		MergedAnnotation<A> annotation = MergedAnnotation.missing();
+		if (factory != null) {
+			annotation = MergedAnnotations.from(factory, SearchStrategy.TYPE_HIERARCHY).get(annotationType);
+		}
+		if (!annotation.isPresent()) {
+			annotation = MergedAnnotations.from(type, SearchStrategy.TYPE_HIERARCHY).get(annotationType);
+		}
+		if (!annotation.isPresent() && AopUtils.isAopProxy(instance)) {
+			annotation = MergedAnnotations.from(AopUtils.getTargetClass(instance), SearchStrategy.TYPE_HIERARCHY)
+					.get(annotationType);
+		}
+		return annotation.isPresent() ? annotation.synthesize() : null;
+	}
+
+	private static boolean isBindableConstructor(Constructor<?> constructor) {
+		Class<?> declaringClass = constructor.getDeclaringClass();
+		Constructor<?> bindConstructor = BindMethod.findBindConstructor(declaringClass);
+		if (bindConstructor != null) {
+			return bindConstructor.equals(constructor);
+		}
+		return BindMethod.forClass(declaringClass) == BindMethod.VALUE_OBJECT;
+	}
+
+	/**
+	 * The binding method that use used for the bean.
+	 */
+	public enum BindMethod {
+
+		/**
+		 * Java Bean using getter/setter binding.
+		 */
+		JAVA_BEAN,
+
+		/**
+		 * Value object using constructor binding.
+		 */
+		VALUE_OBJECT;
+
+		static BindMethod forClass(Class<?> type) {
+			if (MergedAnnotations.from(type, SearchStrategy.TYPE_HIERARCHY).isPresent(ConstructorBinding.class)
+					|| findBindConstructor(type) != null) {
+				return VALUE_OBJECT;
 			}
+			return JAVA_BEAN;
 		}
-		A annotation = AnnotationUtils.findAnnotation(bean.getClass(), annotationType);
-		if (annotation != null) {
-			return annotation;
+
+		static Constructor<?> findBindConstructor(Class<?> type) {
+			if (KotlinDetector.isKotlinPresent() && KotlinDetector.isKotlinType(type)) {
+				Constructor<?> constructor = BeanUtils.findPrimaryConstructor(type);
+				if (constructor != null) {
+					return findBindConstructor(type, constructor);
+				}
+			}
+			return findBindConstructor(type, type.getDeclaredConstructors());
 		}
-		if (AopUtils.isAopProxy(bean)) {
-			return AnnotationUtils.findAnnotation(AopUtils.getTargetClass(bean), annotationType);
+
+		private static Constructor<?> findBindConstructor(Class<?> type, Constructor<?>... candidates) {
+			Constructor<?> constructor = null;
+			for (Constructor<?> candidate : candidates) {
+				if (MergedAnnotations.from(candidate).isPresent(ConstructorBinding.class)) {
+					Assert.state(candidate.getParameterCount() > 0,
+							type.getName() + " declares @ConstructorBinding on a no-args constructor");
+					Assert.state(constructor == null,
+							type.getName() + " has more than one @ConstructorBinding constructor");
+					constructor = candidate;
+				}
+			}
+			return constructor;
 		}
-		return null;
+
 	}
 
 }

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/ConfigurationPropertiesBeanRegistrar.java
@@ -15,16 +15,13 @@
  */
 package org.springframework.boot.context.properties;
 
-import java.lang.reflect.Constructor;
-
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.HierarchicalBeanFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
-import org.springframework.core.KotlinDetector;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBean.BindMethod;
 import org.springframework.core.annotation.MergedAnnotation;
 import org.springframework.core.annotation.MergedAnnotations;
 import org.springframework.core.annotation.MergedAnnotations.SearchStrategy;
@@ -91,23 +88,12 @@ final class ConfigurationPropertiesBeanRegistrar {
 	}
 
 	private BeanDefinition createBeanDefinition(String beanName, Class<?> type) {
-		if (isValueObject(type)) {
+		if (BindMethod.forClass(type) == BindMethod.VALUE_OBJECT) {
 			return new ConfigurationPropertiesValueObjectBeanDefinition(this.beanFactory, beanName, type);
 		}
 		GenericBeanDefinition definition = new GenericBeanDefinition();
 		definition.setBeanClass(type);
 		return definition;
-	}
-
-	private boolean isValueObject(Class<?> type) {
-		if (KotlinDetector.isKotlinPresent() && KotlinDetector.isKotlinType(type)) {
-			Constructor<?> primaryConstructor = BeanUtils.findPrimaryConstructor(type);
-			if (primaryConstructor != null) {
-				return primaryConstructor.getParameterCount() > 0;
-			}
-		}
-		Constructor<?>[] constructors = type.getDeclaredConstructors();
-		return constructors.length == 1 && constructors[0].getParameterCount() > 0;
 	}
 
 }

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/ConfigurationPropertiesBindException.java
@@ -29,19 +29,11 @@ import org.springframework.util.ClassUtils;
  */
 public class ConfigurationPropertiesBindException extends BeanCreationException {
 
-	private final Class<?> beanType;
-
-	private final ConfigurationProperties annotation;
+	private final ConfigurationPropertiesBean bean;
 
 	ConfigurationPropertiesBindException(ConfigurationPropertiesBean bean, Exception cause) {
-		this(bean.getName(), bean.getInstance().getClass(), bean.getAnnotation(), cause);
-	}
-
-	ConfigurationPropertiesBindException(String beanName, Class<?> beanType, ConfigurationProperties annotation,
-			Exception cause) {
-		super(beanName, getMessage(beanType, annotation), cause);
-		this.beanType = beanType;
-		this.annotation = annotation;
+		super(bean.getName(), getMessage(bean), cause);
+		this.bean = bean;
 	}
 
 	/**
@@ -49,7 +41,7 @@ public class ConfigurationPropertiesBindException extends BeanCreationException 
 	 * @return the bean type
 	 */
 	public Class<?> getBeanType() {
-		return this.beanType;
+		return this.bean.getType();
 	}
 
 	/**
@@ -57,13 +49,14 @@ public class ConfigurationPropertiesBindException extends BeanCreationException 
 	 * @return the configuration properties annotation
 	 */
 	public ConfigurationProperties getAnnotation() {
-		return this.annotation;
+		return this.bean.getAnnotation();
 	}
 
-	private static String getMessage(Class<?> beanType, ConfigurationProperties annotation) {
+	private static String getMessage(ConfigurationPropertiesBean bean) {
+		ConfigurationProperties annotation = bean.getAnnotation();
 		StringBuilder message = new StringBuilder();
 		message.append("Could not bind properties to '");
-		message.append(ClassUtils.getShortName(beanType)).append("' : ");
+		message.append(ClassUtils.getShortName(bean.getType())).append("' : ");
 		message.append("prefix=").append(annotation.prefix());
 		message.append(", ignoreInvalidFields=").append(annotation.ignoreInvalidFields());
 		message.append(", ignoreUnknownFields=").append(annotation.ignoreUnknownFields());

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/ConfigurationPropertiesBinder.java
@@ -45,7 +45,6 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.env.PropertySources;
-import org.springframework.util.Assert;
 import org.springframework.validation.Validator;
 import org.springframework.validation.annotation.Validated;
 
@@ -83,22 +82,18 @@ class ConfigurationPropertiesBinder {
 		this.jsr303Present = ConfigurationPropertiesJsr303Validator.isJsr303Present(applicationContext);
 	}
 
-	<T> BindResult<T> bind(Bindable<T> target) {
-		ConfigurationProperties annotation = getAnnotation(target);
+	BindResult<?> bind(ConfigurationPropertiesBean propertiesBean) {
+		Bindable<?> target = propertiesBean.asBindTarget();
+		ConfigurationProperties annotation = propertiesBean.getAnnotation();
 		BindHandler bindHandler = getBindHandler(target, annotation);
 		return getBinder().bind(annotation.prefix(), target, bindHandler);
 	}
 
-	<T> T bindOrCreate(Bindable<T> target) {
-		ConfigurationProperties annotation = getAnnotation(target);
+	Object bindOrCreate(ConfigurationPropertiesBean propertiesBean) {
+		Bindable<?> target = propertiesBean.asBindTarget();
+		ConfigurationProperties annotation = propertiesBean.getAnnotation();
 		BindHandler bindHandler = getBindHandler(target, annotation);
 		return getBinder().bindOrCreate(annotation.prefix(), target, bindHandler);
-	}
-
-	private <T> ConfigurationProperties getAnnotation(Bindable<?> target) {
-		ConfigurationProperties annotation = target.getAnnotation(ConfigurationProperties.class);
-		Assert.state(annotation != null, () -> "Missing @ConfigurationProperties on " + target);
-		return annotation;
 	}
 
 	private Validator getConfigurationPropertiesValidator(ApplicationContext applicationContext) {

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/ConfigurationPropertiesBindingPostProcessor.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBean.BindMethod;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.Ordered;
@@ -92,17 +93,22 @@ public class ConfigurationPropertiesBindingPostProcessor
 
 	@Override
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-		ConfigurationPropertiesBean configurationPropertiesBean = ConfigurationPropertiesBean
-				.get(this.applicationContext, bean, beanName);
-		if (configurationPropertiesBean != null && !hasBoundValueObject(beanName)) {
-			try {
-				this.binder.bind(configurationPropertiesBean.asBindTarget());
-			}
-			catch (Exception ex) {
-				throw new ConfigurationPropertiesBindException(configurationPropertiesBean, ex);
-			}
-		}
+		bind(ConfigurationPropertiesBean.get(this.applicationContext, bean, beanName));
 		return bean;
+	}
+
+	private void bind(ConfigurationPropertiesBean bean) {
+		if (bean == null || hasBoundValueObject(bean.getName())) {
+			return;
+		}
+		Assert.state(bean.getBindMethod() == BindMethod.JAVA_BEAN, "Cannot bind @ConfigurationProperties for bean '"
+				+ bean.getName() + "'. Ensure that @ConstructorBinding has not been applied to regular bean");
+		try {
+			this.binder.bind(bean);
+		}
+		catch (Exception ex) {
+			throw new ConfigurationPropertiesBindException(bean, ex);
+		}
 	}
 
 	private boolean hasBoundValueObject(String beanName) {

commit 92365db9c7b6c4add03d7b3cc82dc00997e5574c

Prevent ValueBinder from being incorrectly used
Fix a regression caused by e41c5a43 which incorrectly allowed the
`ValueObjectBinder` to be picked when the `JavaBeanBinder` should
have been used.

Closes gh-18485

spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/ValueObjectBinder.java
@@ -203,13 +203,12 @@ class ValueObjectBinder implements DataObjectBinder {
 					constructor = candidate;
 				}
 			}
-			return (constructor != null) ? new DefaultValueObject<>((Constructor<T>) constructor) : null;
+			return get((Constructor<T>) constructor);
 		}
 
 		private static boolean isCandidateConstructor(Constructor<?> candidate, Predicate<Constructor<?>> filter) {
 			int modifiers = candidate.getModifiers();
-			return !Modifier.isPrivate(modifiers) && !Modifier.isProtected(modifiers)
-					&& candidate.getParameterCount() > 0 && filter.test(candidate);
+			return !Modifier.isPrivate(modifiers) && !Modifier.isProtected(modifiers) && filter.test(candidate);
 		}
 
 		static <T> DefaultValueObject<T> get(Constructor<T> constructor) {

spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/ConfigurationPropertiesTests.java
@@ -807,6 +807,35 @@ class ConfigurationPropertiesTests {
 		assertThat(bean.getNested().getAge()).isEqualTo(5);
 	}
 
+	@Test // gh-18485
+	void loadWhenBindingToMultiConstructorConfigurationProperties() {
+		MutablePropertySources sources = this.context.getEnvironment().getPropertySources();
+		Map<String, Object> source = new HashMap<>();
+		source.put("test.nested[0].name", "spring");
+		source.put("test.nested[0].age", "5");
+		sources.addLast(new MapPropertySource("test", source));
+		load(MultiConstructorConfigurationPropertiesConfiguration.class);
+		MultiConstructorConfigurationListProperties bean = this.context
+				.getBean(MultiConstructorConfigurationListProperties.class);
+		MultiConstructorConfigurationProperties nested = bean.getNested().get(0);
+		assertThat(nested.getName()).isEqualTo("spring");
+		assertThat(nested.getAge()).isEqualTo(5);
+	}
+
+	@Test // gh-18485
+	void loadWhenBindingToMultiConstructorConfigurationPropertiesUsingShortcutSyntax() {
+		MutablePropertySources sources = this.context.getEnvironment().getPropertySources();
+		Map<String, Object> source = new HashMap<>();
+		source.put("test.nested[0]", "spring");
+		sources.addLast(new MapPropertySource("test", source));
+		load(MultiConstructorConfigurationPropertiesConfiguration.class);
+		MultiConstructorConfigurationListProperties bean = this.context
+				.getBean(MultiConstructorConfigurationListProperties.class);
+		MultiConstructorConfigurationProperties nested = bean.getNested().get(0);
+		assertThat(nested.getName()).isEqualTo("spring");
+		assertThat(nested.getAge()).isEqualTo(0);
+	}
+
 	private AnnotationConfigApplicationContext load(Class<?> configuration, String... inlinedProperties) {
 		return load(new Class<?>[] { configuration }, inlinedProperties);
 	}
@@ -1965,6 +1994,23 @@ class ConfigurationPropertiesTests {
 			}
 
 		}
+
+	}
+
+	@ConfigurationProperties("test")
+	static class MultiConstructorConfigurationListProperties {
+
+		private List<MultiConstructorConfigurationProperties> nested = new ArrayList<>();
+
+		List<MultiConstructorConfigurationProperties> getNested() {
+			return this.nested;
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@EnableConfigurationProperties(MultiConstructorConfigurationListProperties.class)
+	static class MultiConstructorConfigurationPropertiesConfiguration {
 
 	}
 

spring-boot-project/spring-boot/src/test/java/org/springframework/boot/context/properties/MultiConstructorConfigurationProperties.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.context.properties;
+
+/**
+ * Class used to test multi-constructor binding. Must be public and have public
+ * constructors.
+ *
+ * @author Phillip Webb
+ */
+public class MultiConstructorConfigurationProperties {
+
+	private String name;
+
+	private int age;
+
+	public MultiConstructorConfigurationProperties() {
+	}
+
+	public MultiConstructorConfigurationProperties(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}